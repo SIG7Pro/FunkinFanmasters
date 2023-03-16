c2342d18528eec9cf6380c0c0af12e2396ec8d85
SIG7 <82591291+SIG7Pro@users.noreply.github.com>
2023-03-16 13:17:10 -0400

    Reverted back to OG code but I made a minor edit.

--- source/gameObjects/userInterface/HealthIcon.hx
+++ source/gameObjects/userInterface/HealthIcon.hx
@@ -2,7 +2,6 @@
 //To do
 //:
 //Figure out a way to see how wide a sprite's image is, so we can do some shenanigans...
-
 package gameObjects.userInterface;
 
 import flixel.FlxG;
@@ -19,7 +18,6 @@ class HealthIcon extends FlxSprite
 	public var initialWidth:Float = 0;
 	public var initialHeight:Float = 0;
 	public var isOldIcon:Bool = false;
-
 
 	public function new(char:String = 'bf', isPlayer:Bool = false)
 	{
@@ -40,7 +38,7 @@ class HealthIcon extends FlxSprite
 			else
 				iconPath = 'face';
 			trace('$char icon trying $iconPath instead you fuck');
-	}
+		}
 
 		antialiasing = true;
 		var iconGraphic:FlxGraphic = Paths.image('icons/icon-' + iconPath);
@@ -48,15 +46,13 @@ class HealthIcon extends FlxSprite
 
 		initialWidth = width;
 		initialHeight = height;
-//Codename Engine Shenanigans until ya set the scrollfactor.
-		animation.add(char, [for(i in 0...frames.frames.length) i], 0, false, isPlayer);
-		antialiasing = true;
-		animation.play(char);
-
-		if (frames.frames.length >= 3)
-			healthSteps[80] = 2; // winning icon
-
-
+		
+         if (frames.frames.length <= 3)
+		animation.add('icon', [0, 1], 0, false, isPlayer);
+	 else
+		animation.add('icon', [0, 1, 2], 0, false, isPlayer);
+		
+		animation.play('icon');
 		scrollFactor.set();
 	}
 
@@ -71,13 +67,13 @@ class HealthIcon extends FlxSprite
 
 //Funkin' Lullaby Shenanigans
 //thanks to not billy for correcting the line below by stating i didn't need to make the line below public or something for a module lewel field
-	function swapOldIcon()
-	{
-		if (isOldIcon = !isOldIcon)
-			changeIcon('bf-old');
-		else
-			changeIcon('bf');
-	}
+	//function swapOldIcon()
+	//{
+	//	if (isOldIcon = !isOldIcon)
+	//		changeIcon('bf-old');
+	//	else
+	//		changeIcon('bf');
+	//}
 
 
 
